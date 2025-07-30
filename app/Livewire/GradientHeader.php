bb1cdc5d7329ef8287eb8db2e6b57fee02d5b2d9
000SMS000 <mahmoud.m.m.ahmed2017@gmail.com>
2025-07-30 15:20:40 +0300

    4th commit

---
+++ app/Livewire/GradientHeader.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class GradientHeader extends Component
+{
+    public function render()
+    {
+        return view('livewire.gradient-header');
+    }
+}
---
+++ resources/views/livewire/gradient-header.blade.php
@@ -0,0 +1,1 @@
+<div class="h-14 bg-gradient-to-r from-cyan-500 to-blue-500 flex items-center px-4 text-white font-semibold shadow"></div>
