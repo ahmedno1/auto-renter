0b2c9e17c7cd80288470ba84a596594319e9c1cf
000SMS000 <mahmoud.m.m.ahmed2017@gmail.com>
2025-08-03 16:23:03 +0300

    database & the migration of user taple

--- auto-renter/resources/views/livewire/auth/register.blade.php
+++ auto-renter/resources/views/livewire/auth/register.blade.php
@@ -1,5 +1,46 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Livewire\Attributes\Layout;
+use Livewire\Volt\Component;
+
+new #[Layout('components.layouts.auth')] class extends Component {
+    public string $name = '';
+    public string $email = '';
+    public string $password = '';
+    public string $password_confirmation = '';
+    public string $phone='';
+    public string $role='';
+
+    /**
+     * Handle an incoming registration request.
+     */
+    public function register(): void
+    {
+        $validated = $this->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+            'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
+            'phone'=>['required','string'],
+            'role'=>['required','string'],
+        ]);
+
+        $validated['password'] = Hash::make($validated['password']);
+
+        event(new Registered(($user = User::create($validated))));
+
+        Auth::login($user);
+
+        $this->redirectIntended(route('dashboard', absolute: false), navigate: true);
+    }
+}; ?>
+
 <div class="flex flex-col gap-6">
-    <x-auth-header :title="__('Create an account')" :description="__('Enter your details below to create your account')" />
+    <x-auth-header :title="('Create an account')" :description="('Enter your details below to create your account')" />
 
     <!-- Session Status -->
     <x-auth-session-status class="text-center" :status="session('status')" />
@@ -8,18 +49,18 @@
         <!-- Name -->
         <flux:input
             wire:model="name"
-            :label="__('Name')"
+            :label="('Name')"
             type="text"
             required
             autofocus
             autocomplete="name"
-            :placeholder="__('Full name')"
+            :placeholder="('Full name')"
         />
 
         <!-- Email Address -->
         <flux:input
             wire:model="email"
-            :label="__('Email address')"
+            :label="('Email address')"
             type="email"
             required
             autocomplete="email"
@@ -29,24 +70,41 @@
         <!-- Password -->
         <flux:input
             wire:model="password"
-            :label="__('Password')"
+            :label="('Password')"
             type="password"
             required
             autocomplete="new-password"
-            :placeholder="__('Password')"
+            :placeholder="('Password')"
             viewable
         />
 
         <!-- Confirm Password -->
         <flux:input
             wire:model="password_confirmation"
-            :label="__('Confirm password')"
+            :label="('Confirm password')"
             type="password"
             required
             autocomplete="new-password"
-            :placeholder="__('Confirm password')"
+            :placeholder="('Confirm password')"
             viewable
         />
+
+        <!-- phone number -->
+        <flux:input
+            wire:model="phone"
+            :label="('phone number')"
+            type="text"
+            required
+            autocomplete="phone number"
+            :placeholder="('phone number')"
+        />
+        
+        <!-- role -->
+        <flux:radio.group wire:model="role" :label="('role')" variant="segmented">
+            <flux:radio label="Owner" />
+            <flux:radio label="user" />
+        </flux:radio.group>
+         
 
         <div class="flex items-center justify-end">
             <flux:button type="submit" variant="primary" class="w-full">
@@ -56,8 +114,7 @@
     </form>
 
     <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-600 dark:text-zinc-400">
-        <flux:button x-data x-on:click="$flux.dark = ! $flux.dark" icon="moon" variant="subtle" aria-label="Toggle dark mode" />
         <span>{{ __('Already have an account?') }}</span>
         <flux:link :href="route('login')" wire:navigate>{{ __('Log in') }}</flux:link>
-    </div>
+    </div>
 </div>
