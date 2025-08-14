9815b58931b64cf7e39229686a6b022ac44e4db1
000SMS000 <mahmoud.m.m.ahmed2017@gmail.com>
2025-08-14 15:02:39 +0300

    ඞ

--- auto-renter/resources/views/components/layouts/app/header.blade.php
+++ auto-renter/resources/views/components/layouts/app/header.blade.php
@@ -7,7 +7,7 @@
         <flux:header container class="border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
             <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
 
-            <a href="{{ route('dashboard') }}" class="me-5 flex items-center space-x-2 rtl:space-x-reverse hidden dark:flex" wire:navigate>
+            <a href="{{ route('dashboard') }}" class="me-5 items-center space-x-2 rtl:space-x-reverse hidden dark:flex" wire:navigate>
                 <x-app-logo />
             </a>
 
