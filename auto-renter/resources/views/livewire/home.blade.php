8e4b5ce3e58cd08cf65d39b58f3d72163adc2e8c
000SMS000 <mahmoud.m.m.ahmed2017@gmail.com>
2025-08-14 15:55:37 +0300

    start in home page

--- auto-renter/resources/views/livewire/home.blade.php
+++ auto-renter/resources/views/livewire/home.blade.php
@@ -1,5 +1,31 @@
 <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-    <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-        <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+<section class="relative bg-black text-white">
+    <div class="container mx-auto px-6 lg:px-16 py-16 flex flex-col-reverse lg:flex-row items-center gap-12">
+        <div class="flex-1 text-center lg:text-left">
+            <h1 class="text-4xl md:text-5xl lg:text-6xl font-bold leading-tight">
+                Rent the Best Car <br> Around the World
+            </h1>
+            <p class="mt-4 text-lg text-gray-300">
+                We provide the best car options and expert services to ensure the greatest customer experience.
+            </p>
+            <div class="mt-6 flex flex-wrap justify-center lg:justify-start gap-4">
+                <a href="#cars" class="bg-blue-600 hover:bg-blue-700 px-6 py-3 rounded-full shadow-lg transition">
+                    Rent a Car
+                </a>
+                <a href="#learn-more" class="border border-gray-400 hover:border-white px-6 py-3 rounded-full transition">
+                    Learn More
+                </a>
+            </div>
+        </div>
+        <div class="flex-1 relative">
+            <img src="/image/car.png" alt="Car" class="w-full max-w-5xl mx-auto relative z-10 ">
+        </div>
     </div>
+
+    <svg class="absolute  top-170 z-0 inset-0 w-full h-[200px]" viewBox="0 0 1440 520" preserveAspectRatio="none" aria-hidden="true">
+    <path d="M0,0 H1440 V320
+             C1080,520 360,520 0,320
+             Z" fill="black"/>
+  </svg>
+</section>
 </div>
